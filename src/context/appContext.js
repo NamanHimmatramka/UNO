dc0b70847e3e30af3f6da18e3f31937dde882409
Naman Himmatramka <himmatramka2003@gmail.com>
2022-11-21 23:06:29 +0530

    socket url changed

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "http://localhost:3001";
+const SOCKET_URL = "https://arcane-badlands-93459.herokuapp.com";
 export const socket = io(SOCKET_URL);
 //app context
 export const AppContext = React.createContext();
